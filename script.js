a45c80a957a48273022e00a26fe3020deca0ad23
wxlyan <officialdarvaluwp@gmail.com>
2026-06-05 02:14:29 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const tabs = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.tab-content');
 
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+
+    contents.forEach(c => c.classList.remove('active'));
+    document.getElementById(tab.dataset.tab).classList.add('active');
+  });
+});
